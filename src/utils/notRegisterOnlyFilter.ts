d46ddcfc43507dd68f373a9f73e164f27397c60d
rinsuki <6533808+rinsuki@users.noreply.github.com>
2017-11-18 04:21:40 +0900

    fix

--- src/utils/notRegisterOnlyFilter.ts
+++ src/utils/notRegisterOnlyFilter.ts
@@ -1,9 +1,10 @@
 import * as Koa from "koa"
 import {User} from "../models"
+import { FlashTypes } from "./flash";
 
 export default async (ctx: Koa.Context, next: any) => {
     if (await User.find().count()) {
-        ctx.flash("アカウントは既に登録済みです。ログインしてください", "error")
+        ctx.flash("アカウントは既に登録済みです。ログインしてください", FlashTypes.error)
         ctx.redirect("/login")
         return
     }
