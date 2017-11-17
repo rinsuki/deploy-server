9b8eb72ba8c5a4a2e6aa1ba01201736c637cff2b
rinsuki <6533808+rinsuki@users.noreply.github.com>
2017-11-17 21:17:11 +0900

    wip

--- src/utils/pugView.ts
+++ src/utils/pugView.ts
@@ -4,7 +4,7 @@ const path = require("path")
 module.exports = async (ctx, next) => {
     ctx.render = (view, params = {}) => {
         params.state = ctx.state
-        ctx.body = pug.renderFile(path.join(__dirname, "..", "views", view)+".pug", params)
+        ctx.body = pug.renderFile(path.join(__dirname, "..", "..", "views", view)+".pug", params)
     }
     await next()
 }
